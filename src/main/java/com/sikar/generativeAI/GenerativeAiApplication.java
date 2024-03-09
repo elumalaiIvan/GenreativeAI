793241ff3e15f4a7f01f924101005100042ba9e9
Elumalai Ramalingam <mynameiselumalai@gmail.com>
2024-03-09 13:39:39 -0500

    basic structure and dependencies

---
+++ src/main/java/com/sikar/generativeAI/GenerativeAiApplication.java
@@ -0,0 +1,14 @@
+package com.sikar.generativeAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerativeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerativeAiApplication.class, args);
+	}
+
+}
+
